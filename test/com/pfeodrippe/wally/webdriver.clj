5b9d07bd280d5573bcaaa251b3b0dbc13712f6ca
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-04-10 00:21:39 -0400

    wip2

--- test/com/pfeodrippe/wally/webdriver.clj
+++ test/com/pfeodrippe/wally/webdriver.clj
@@ -1,16 +1,18 @@
 (ns com.pfeodrippe.wally.webdriver
   (:require
+   [clojure.string :as str]
    [clojure.test :refer [deftest testing is use-fixtures]]
    [garden.selectors :as s]
-   [wally.main :as w]
-   [wally.selectors :as ws]
    [recife.analyzer :as ra]
    [recife.core :as r]
    [recife.helpers :as rh]
-   [recife.webdriver :as rw]))
+   [recife.webdriver :as rw]
+   [wally.main :as w]
+   [wally.selectors :as ws]))
 
 (def global
-  {::playing? false})
+  {::playing? false
+   ::time 0})
 
 (r/defproc play
   (fn [{::keys [playing?] :as db}]
@@ -22,25 +24,63 @@
     (when playing?
       (assoc db ::playing? false))))
 
+(r/defproc tick
+  (fn [{::keys [playing?] :as db}]
+    (when playing?
+      (update db ::time inc))))
+
+;; We add a constraint so ◊code{time} does not evolve forever.
+(rh/defconstraint time-constraint
+  [{::keys [time]}]
+  (< time 10))
+
 (comment
 
-  @(r/run-model global #{play pause} {:trace-example true})
-  (r/states-from-result (r/get-result))
-  (r/random-traces-from-result (r/get-result) {:max-number-of-traces 10
+  @(r/run-model global #{play pause tick time-constraint} {:trace-example true})
+  (r/random-traces-from-result (r/get-result) {:max-number-of-traces 1
                                                :max-number-of-states 10})
 
   (let [init (fn  [_]
                (w/navigate "file:///Users/paulo.feodrippe/dev/wally-tests/audioplayer.html"))
+
         procs-mapping {::play
-                       {:handler (fn [_]
-                                   (w/click :play-pause))}}
+                       (fn [_]
+                         (w/click :play-pause))
+
+                       ::pause
+                       (fn [_]
+                         (w/click :play-pause))
+
+                       ::tick
+                       (fn [_]
+                         (Thread/sleep 1000))}
+
         states-mapping {::playing?
-                        (fn [aa]
-                          (def aa aa)
-                          true)}]
-    (rw/analyze (r/get-result) {:init init
-                                :procs-mapping procs-mapping
-                                :states-mapping states-mapping}))
+                        (fn [_ {::keys [playing?]}]
+                          (if playing?
+                            (is (= "Pause" (w/text-content :play-pause)))
+                            (is (= "Play" (w/text-content :play-pause)))))
+
+                        ::time
+                        {:assertion
+                         (fn [previous-db db]
+                           (if-let [previous-time (-> previous-db :_impl ::time)]
+                             (<= previous-time (-> db :_impl ::time))
+                             true))
+
+                         :save
+                         (fn [_]
+                           (let [[minutes seconds] (->> (-> (w/text-content :time-display)
+                                                            (str/split #":"))
+                                                        (mapv #(Integer/parseInt %)))]
+                             (+ (* 60 minutes) seconds)))}}]
+
+    (rw/analyze (r/get-result)
+                {:init init
+                 :procs-mapping procs-mapping
+                 :states-mapping states-mapping
+                 :max-number-of-traces 1
+                 :max-number-of-states 10}))
 
   ())
 
@@ -49,4 +89,16 @@
 ;;   - [x] Play
 ;;   - [x] Pause
 ;;   - [ ] Tick
+;;     - [ ] We may need something to tell us about old state (in the implemenation)
+;;           so we can use invariants
 ;; - [ ] Wait on event
+;; - [ ] Could we run it from TLC?
+
+;; From https://arxiv.org/pdf/2203.11532.pdf.
+;; -------------------------------------------------
+;; Currently, the Quickstrom checker makes a completely
+;; random selection from the set of allowable actions for the
+;; current state. Refining this action selection to be more targeted, methodically exploring previously unreached parts of
+;; the state space, is left as future work (see Section 5.1).
+
+;; Check model inference papers on section 5.1 (p. 11)
--- test/recife/webdriver.clj
+++ test/recife/webdriver.clj
@@ -1,8 +1,9 @@
 (ns recife.webdriver
   "Functions to help you webdrive a browser using Recife."
   (:require
-   [recife.core :as r]
-   [clojure.test :refer [is testing report]]))
+   [clojure.test :refer [is testing report]]
+   [medley.core :as m]
+   [recife.core :as r]))
 
 (comment
 
@@ -26,20 +27,23 @@
   report)
 
 (defn check-state
-  [state states-mapping]
+  [previous-state state states-mapping]
   (->> states-mapping
-       (mapv (fn [[k handler]]
+       (mapv (fn [[k {:keys [assertion save]}]]
                [k (if (contains? state k)
                     (let [*test-info (atom [])]
                       (with-redefs [report (fn [event]
                                              (swap! *test-info conj
                                                     {:event event
-                                                     :testing-contexts (vec clojure.test/*testing-contexts*)})
+                                                     :testing-contexts
+                                                     (vec (reverse clojure.test/*testing-contexts*))})
                                              (original-report event))]
                         (testing k
-                          (let [result (handler (get state k))]
+                          (def fgg assertion)
+                          (let [result (assertion previous-state state)]
                             (if result
-                              {:result result}
+                              {:result result
+                               :impl-state (save state)}
                               {:result result
                                :test-info @*test-info})))))
                     {:result true})]))
@@ -50,27 +54,27 @@
   [trace {:keys [init procs-mapping states-mapping]}]
   (let [initial-global-state (dissoc (get-in trace [0 1]) ::r/procs)
         _ (init initial-global-state)]
-    (loop [[[idx {:keys [recife/metadata] :as state}] & next-states]
-           (take 2 trace)
-
+    (loop [[[idx {:keys [recife/metadata] :as state}] & next-states] trace
            history []]
       (when idx
         (if metadata
-          (let [{:keys [context]} metadata
+          (let [previous-state (last (last history))
+                {:keys [context]} metadata
                 step (first context)
                 step-fn (procs-mapping step)]
             (when-not step-fn
               (throw (ex-info "Handler not implemented for step" {:step step})))
             (step-fn context)
-            (let [errors (check-state state states-mapping)]
+            (let [errors (check-state previous-state state states-mapping)]
               (if (seq errors)
                 {:type :violation
                  :step step
                  :errors errors
                  :trace history}
-                (recur next-states (conj history)))))
+                (recur next-states (conj history [idx state])))))
 
-          (let [errors (check-state state states-mapping)]
+          ;; Initial state.
+          (let [errors (check-state nil state states-mapping)]
             (if (seq errors)
               {:type :initial-state-violation
                :errors errors
@@ -79,40 +83,34 @@
 
 #_{:clj-kondo/ignore [:unused-binding]}
 (defn analyze
-  [result {:keys [init procs states max-number-of-traces]
-           :or {max-number-of-traces 20}
+  "`max-number-of-states` drives how "
+  [result {:keys [init procs states max-number-of-traces max-number-of-states]
+           :or {max-number-of-traces 10
+                max-number-of-states 10}
            :as params}]
-  (loop [[trace & other-traces] (-> result
-                                    r/states-from-result
-                                    (r/random-traces-from-states
-                                     {:max-number-of-traces max-number-of-traces
-                                      :max-number-of-states 10}))
-         trace-counter 0]
-    (if trace
-      (let [result (analyze-trace trace params)]
-        (recur other-traces (inc trace-counter)))
-      {:ok :ok})))
-
-(comment
-
-  (do
-    (require '[wally.main :as w])
-    (def init
-      (fn  [_]
-        (w/navigate "file:///Users/paulo.feodrippe/dev/wally-tests/audioplayer.html")))
-
-    (def procs-mapping {:com.pfeodrippe.wally.webdriver/play
-                        (fn [_]
-                          (w/click :play-pause))
-
-                        :com.pfeodrippe.wally.webdriver/pause
-                        (fn [_]
-                          (w/click :play-pause))})
-
-    (def states-mapping {:com.pfeodrippe.wally.webdriver/playing?
-                         (fn [v]
-                           (if v
-                             (is (= "Pause" (w/text-content :play-pause)))
-                             (is (= "Play" (w/text-content :play-pause)))))}))
-
-  ())
+  (let [params (update params
+                       :states-mapping
+                       ;; Adapt states mapping.
+                       (fn [states-mapping]
+                         (->> states-mapping
+                              (m/map-vals (fn [{:keys [assertion save]
+                                                :or {assertion (constantly true)
+                                                     save identity}
+                                                :as v}]
+                                            (if (fn? v)
+                                              {:assertion v
+                                               :save identity}
+                                              {:assertion assertion
+                                               :save identity}))))))]
+    (loop [[trace & other-traces] (-> result
+                                      r/states-from-result
+                                      (r/random-traces-from-states
+                                       {:max-number-of-traces max-number-of-traces
+                                        :max-number-of-states max-number-of-states}))
+           trace-counter 0]
+      (if trace
+        (let [result (analyze-trace trace params)]
+          (if (:errors result)
+            result
+            (recur other-traces (inc trace-counter))))
+        {:ok :ok}))))
